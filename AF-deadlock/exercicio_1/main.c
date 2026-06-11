32f3257b9c5d99fc06770e9ad40409c7f1a807fd
leoliveirak <klitzkel789@gmail.com>
2026-06-10 21:04:07 -0300

    resolve race condition

--- AF-deadlock/exercicio_1/main.c
+++ AF-deadlock/exercicio_1/main.c
@@ -24,18 +24,15 @@ void *worker1_func(void *arg) {
     for (int i = 0; i < WORKER_LOOPS; ++i) {
         sem_wait(&sem_lista);
         printf("Worker 1 obteve sem_lista\n");
-
         int operacao = operacao_worker1();
-        lista_de_operacoes[proximo_indice++] = operacao;
-
-        sem_post(&sem_lista);
-        printf("Worker 1 liberou sem_lista\n");
-
         pthread_mutex_lock(&mutex_total);
         printf("Worker 1 obteve mutex_total\n");
 
         total_computado += operacao;
+        lista_de_operacoes[proximo_indice++] = operacao;
 
+        sem_post(&sem_lista);
+        printf("Worker 1 liberou sem_lista\n");
         pthread_mutex_unlock(&mutex_total);
         printf("Worker 1 liberou mutex_total\n");
     }
@@ -45,16 +42,14 @@ void *worker1_func(void *arg) {
 
 void *worker2_func(void *arg) {
     for (int i = 0; i < WORKER_LOOPS; ++i) {
+        
         sem_wait(&sem_lista);
         printf("Worker 2 obteve sem_lista\n");
-
-        int operacao = operacao_worker2();
-
-        sem_post(&sem_lista);
-        printf("Worker 2 liberou sem_lista\n");
-
+        
         pthread_mutex_lock(&mutex_total);
         printf("Worker 2 obteve mutex_total\n");
+        
+        int operacao = operacao_worker2();
 
         lista_de_operacoes[proximo_indice] = operacao;
         total_computado += lista_de_operacoes[proximo_indice];
@@ -62,6 +57,9 @@ void *worker2_func(void *arg) {
 
         pthread_mutex_unlock(&mutex_total);
         printf("Worker 2 liberou mutex_total\n");
+        
+        sem_post(&sem_lista);
+        printf("Worker 2 liberou sem_lista\n");
     }
     return NULL;
 }
